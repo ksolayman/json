9fe4451654ae9cb4e04506d281d433164b4608eb
ksolayman <ksolayman.cse@gmail.com>
2023-01-06 18:05:35 +0600

    json

---
+++ xindex.js
@@ -0,0 +1,9 @@
+var data = require("./JsonDemo.json");
+// console.log(data);
+
+console.log(data.friends[0]);
+console.log(data.friends[0].name);
+
+console.log(data.friends[0].name = "ZH");
+console.log(data.friends[0]);
+
